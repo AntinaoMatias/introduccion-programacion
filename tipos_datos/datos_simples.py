00f2e562ef7107a235b6cd8b46b39b70ff3272d9
malexizs <m.alexis2203@gmail.com>
2025-06-09 19:47:59 -0400

    Clase 1 tipos de datos

---
+++ tipos_datos/datos_simples.py
@@ -0,0 +1,55 @@
+#Datos de tipo cadena de texto, o string:
+'Cadena de texto'
+"Otra cadena de texto"
+"""Con esta tercer linea de caracteres
+definimos un string con uno o más
+saltos de linea, simplemente comenzando
+y terminando con triple comillas
+Funciona con simples y dobles"""
+
+#Comando print para imprimir información
+print("Esto es una cadena de texto")
+print('Esto es una cadena de texto')
+print('Esto es una cadena de texto con "comillas dobles"')
+print("Esto es una cadena de texto con 'comillas simples'")
+
+#Crear una variable para almacenar strings
+cadena_texto_1 = "Hola, Don Pepito"
+cadena_texto_2 = "Hola, Don José"
+cadena_texto_3 = "¿Pasó usted ya por casa?"
+cadena_texto_4 = "Por su casa yo pasé"
+cadena_texto_5 = "¿Vio usted a mi abuela?"
+cadena_texto_6 = "A su abuela yo la ví"
+despedida1 = "Adiós Don Pepito"
+despedida2 = "Adiós Don José"
+
+print(cadena_texto_1)
+print(cadena_texto_2)
+print(cadena_texto_3)
+print(cadena_texto_4)
+print(cadena_texto_5)
+print(cadena_texto_6)
+print(despedida1)
+print(despedida2)
+
+#Ejemplo 2: Usaremos input() para recibir datos del usuario
+
+saludo = "Buenos días"
+nombre = input("Escriba su nombre: ")
+texto_final = saludo + nombre
+print(texto_final)
+
+#De esta manera escribimos comentarios
+#Estos no se ejecutarán con el código
+
+#Datos de tipo numéricos
+#Tipo int
+numero_entero = 49
+#Tipo float
+numero_decimal = 49.99
+
+#Datos de tipo lógico (booleanos)
+#Tipo bool
+dato_verdadero = True
+dato_falso = False
+
